96aeced977c234d1fba2be31f4c4a595c6c8692f
kezzyhko <s.semushin@innopolis.university>
2019-06-07 17:44:14 +0300

    Made chat reconnect automatically

--- web/a7_xss/static/a7_xss/chat.js
+++ web/a7_xss/static/a7_xss/chat.js
@@ -1,35 +1,46 @@
-var socket = new WebSocket('ws://' + window.location.host + '/chat/');
+var socket;
+function startWebsocket() {
+    socket = new WebSocket('ws://' + window.location.host + '/chat/');
 
-socket.onmessage = function(e) {
-    var response = JSON.parse(e.data);
+    socket.onmessage = function(e) {
+        var response = JSON.parse(e.data);
+        var li = document.createElement("li");
+        li.classList.add(response.type);
+        switch (response.type) {
+            case 'connected':
+                li.innerHTML = '<span class="username">' + response['username'] + '</span> has connected';
+            break;
+            case 'error':
+                li.innerHTML = response['error'];
+            break;
+            case 'message':
+                li.innerHTML = '<span class="username">' + response['username'] + ': </span>';
+                li.append(response['message']);
+            break;
+        }
+        chat.insertBefore(li, chat.children[0])
+    };
 
-    var li = document.createElement("li");
-    li.classList.add(response.type);
-    switch (response.type) {
-        case 'connected':
-            li.innerHTML = '<span class="username">' + response['username'] + '</span> has connected';
-        break;
-        case 'error':
-            li.innerText = response['error'];
-        break;
-        case 'message':
-            li.innerHTML = '<span class="username">' + response['username'] + ': </span>';
-            li.append(response['message']);
-        break;
-    }
+    socket.onclose = function(e) {
+        var li = document.createElement("li");
+        li.classList.add('error');
+        li.innerText = 'Disconnected. Trying to reconnect...';
+        chat.insertBefore(li, chat.children[0])
 
-    var chat = document.getElementById('chat');
-    chat.insertBefore(li, chat.children[0])
-};
+        socket = null;
+        setTimeout(startWebsocket, 5000);
+    };
+}
+startWebsocket();
 
-socket.onclose = function(e) {
-    console.error('Chat socket closed unexpectedly');
-};
 
-document.getElementById('chatform').onsubmit = function(e) {
-	e.preventDefault();
-    var messageEl = document.getElementById('message');
-    socket.send(JSON.stringify({'message': messageEl.value}));
-    messageEl.value = '';
-    messageEl.focus();
-};
+chatform = document.getElementById('chatform');
+if (chatform) {
+    chatform.onsubmit = function(e) {
+    	e.preventDefault();
+        var messageEl = document.getElementById('message');
+        socket.send(JSON.stringify({'message': messageEl.value}));
+        messageEl.value = '';
+        messageEl.focus();
+    };
+}
